51895942f7aeb30d2faa5fd4cbfe0965502e63c4
Edward Lemay <edlemay99@hotmail.com>
2024-10-31 17:16:55 -0400

    GUI: Silent `Loading` and `noscript` fallback - `Loading` now doesn't display anything - Added `noscript` to in in case browser doesn't support JavaScript - Always wondered how the `noscript` element worked, nice to know

--- src/lib/layout/Loading.svelte
+++ src/lib/layout/Loading.svelte
@@ -3,5 +3,15 @@
 </script>
 
 <StageElement static>
-	<p>Loading...</p>
+	<noscript class="flex w-full max-w-2xl flex-col gap-6">
+		<h1 class="text-4xl font-extrabold">
+			<span class="text-yellow-500">!!</span>
+			<span>Limited browser capabilities</span>
+			<span class="text-yellow-500">!!</span>
+		</h1>
+		<p class="text-xl font-bold">
+			JavaScript is currently disabled or unavailable. Please enable it or try using a different
+			browser.
+		</p>
+	</noscript>
 </StageElement>
